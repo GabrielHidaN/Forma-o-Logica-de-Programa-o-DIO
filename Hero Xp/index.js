afb087afa5054dcf1cd3e517deb8464324fd8e31
Gabriel Victor <122997003+GabrielHidaN@users.noreply.github.com>
2024-04-13 20:42:10 -0300

    Hero Challenger

---
+++ Hero Xp/index.js
@@ -0,0 +1,46 @@
+while (true){
+  let nomeHeroi = 'Bradock'
+  let expHeroi = 10001
+  let nivelHeroi = ''
+
+  if (expHeroi < 1000){
+    nivelHeroi = 'Ferro'
+    console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivelHeroi}`)
+    break
+  }
+  else if (expHeroi < 2000){
+    nivelHeroi = 'Bronze'
+    console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivelHeroi}`)
+    break
+  }
+  else if (expHeroi < 5000){
+    nivelHeroi = 'Prata'
+    console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivelHeroi}`)
+    break
+  }
+  else if (expHeroi < 7000){
+    nivelHeroi = 'Ouro'
+    console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivelHeroi}`)
+    break
+  }
+  else if (expHeroi < 8000){
+    nivelHeroi = 'Platina'
+    console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivelHeroi}`)
+    break
+  }
+  else if (expHeroi < 9000){
+    nivelHeroi = 'Ascendente'
+    console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivelHeroi}`)
+    break
+  }
+  else if (expHeroi < 10000){
+    nivelHeroi = 'Imortal'
+    console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivelHeroi}`)
+    break
+  }
+  else {
+    nivelHeroi = 'Radiante'
+    console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivelHeroi}`)
+    break
+  }
+}
